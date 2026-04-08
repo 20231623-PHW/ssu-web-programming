e41f4d3e0ce6d21451a004edee51c1e11df1932d
박현우 <hyuns@hyuns.dev>
2026-04-08 16:19:25 +0900

    feat: about 페이지에 내비게이션 바 추가 및 서버 포트 변경

--- w06/index.js
+++ w06/index.js
@@ -6,10 +6,26 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.post("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.patch("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.put("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.delete("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.get("/about", (req, res) => {
   res.sendFile(__dirname + "/about.html");
 });
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+app.listen(4000, () => {
+  console.log("Example app listening on port 4000!");
 });
